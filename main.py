92a482e40e17ec915154981783df2a83ea7832f8
NeelX10 <neelsuri3009@gmail.com>
2024-10-13 23:58:03 +0200

    Functionality: Can call API, and print JSON data for PM10 as a dictionary

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 
 import requests
 import datetime
+import sys
 
-def api_endpoints(parameter):
-    '''
+# Will be looked into in the future
+
+'''def api_endpoints(parameter):
+    
     This function determines which endpoint that needs to be used when the user calls the API
 
     For example: 1) If the user wants all the measuring station names, then the stations endpoint will be
-    contatenated to the base url.
+    concatenated to the base url.
       
     2) the NO2 values for a day from one particular station would mean that the stations/{stationname}/measurements
     would be used
@@ -19,14 +22,14 @@ def api_endpoints(parameter):
     Output:
         endpoint: string
 
-    '''
+    
     endpoints = ["component","components","organisations","stations",""]
     endpoint = endpoints[parameter]
-    return endpoint
+    return endpoint'''
 
 
 def input_validate(userinp):
-   '''
+    '''
     This function validates the user input for the function user_input as handling incosistent
     user-entered data is essential to ensure the proper/smooth functioning of a program
 
@@ -39,31 +42,85 @@ def input_validate(userinp):
             userinp: string
 
         Else:
-            break the while loop and return to the top of the user_input function
+            return None
     '''
-#    while True:
-       
-#        if type(user_input)
+    if len(userinp) < 1:
+        
+        print("Incorrect input!")
+        return None
+    
+    elif isinstance(userinp,str) and userinp in ["PM10"]:
+        
+        return userinp.upper()
 
-def user_input(base_url = 'https://api.luchtmeetnet.nl/open_api'):
+    #elif isinstance(userinp,datetime):
+
+        #return userinp
+
+    else:
+        
+        print("Incorrect input!")
+        return None
+    
+def station_measurements(param,order="formula",order_dir="asc",station="NL01494"):
     '''
+    This function creates the final piece of the URL based on the input parameter/pollutant, to help make the final url
+    that will be used to call the API. This function will help in essentially getting the pollutant measurements for
+    a particular station 
+
+    Input:
+        param: string
+        order: string
+        station: string
+
+    Output:
+        endpoint_included_url: string
     '''
-    print("Enter which data you need ")
-    parameter = input("Enter Parameter:")
+    endpoint_included_url = f'stations/{station}/measurements?page=1&order={order}&order_direction={order_dir}&formula={param}'
+    return endpoint_included_url
 
 
-    print("Enter which time interval you need the data for")
-    # time_range =
+def user_input(base_url = 'https://api.luchtmeetnet.nl/open_api/'):
+    '''
+    This function asks the user for a parameter/pollutant, then invokes the input validation functions and if the user data
+    is good, then it returns a complete URL needed for calling the API
+    Input:
+        base_url: string
 
-    url = base_url + api_endpoints()
-    return url
+    Output:
+        url: string
+    '''
+    parameter = input("Enter Parameter (Pollutant PM10 will only work for now): ")
+    
+    if input_validate(parameter) is not None:
 
+        # print("Enter which time interval you need the data for")
+        # time_range = input()
+        url = base_url + station_measurements(parameter)
+        return url
+        # url = base_url + api_endpoints()
+        # return url
+    
+    else:
+        
+        return None
 
 def call_api():
-    '''Define the API url reading function
     '''
-    r = requests.get(user_input())
-    print(r.json())
+    Input:
+        No inputs, runs the user_input() function to make the URL for api calling
 
+    Output:
+        dictionary (API response)
+    '''
+    if user_input is not None:
+        r = requests.get(user_input())
+        return r.json()
+    
+    else:
+        print("Hmm something went wrong...please try running the program again")
+        sys.exit()
 
-endpoint = 'components'
+print(call_api())
+# user_input()
+# print(user_input())
